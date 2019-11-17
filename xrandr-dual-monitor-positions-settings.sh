36dee005769d887c7d4e62d72a05067e85f891c8
Arek Czubik <arek.czubik@gmail.com>
2019-11-17 08:13:19 +0100

    i3 workspaces assignment/xrandr placement/vim clipboard

--- xrandr-dual-monitor-positions-settings.sh
+++ xrandr-dual-monitor-positions-settings.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-I-0 --off --output DVI-I-1 --mode 1920x1200 --pos 3840x819 --rotate normal --output HDMI-0 --mode 3840x2160 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DVI-D-0 --off
+xrandr --output DVI-I-0 --off --output DVI-I-1 --mode 1920x1200 --pos 3840x500 --rotate normal --output HDMI-0 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DVI-D-0 --off
